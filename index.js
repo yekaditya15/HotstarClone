6c6c39b04c4eda03ff6936bb23f86f9e15e9628e
yekaditya15 <yekaditya33@gmail.com>
2023-12-27 23:34:55 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,140 @@
+let movies = [
+  {
+    name: "falcon and the winter soldier",
+    des: "Do Watch !!",
+    image: "images/slider 2.PNG",
+  },
+  {
+    name: "loki",
+    des: "Do Watch !!",
+    image: "images/slider 1.PNG",
+  },
+  {
+    name: "wanda vision",
+    des: "Do Watch !!",
+    image: "images/slider 3.PNG",
+  },
+  {
+    name: "raya and the last dragon",
+    des: "Do Watch !!",
+    image: "images/slider 4.PNG",
+  },
+  {
+    name: "luca",
+    des: "Do Watch !!",
+    image: "images/slider 5.PNG",
+  },
+];
+
+const caraousel = document.querySelector(".caraousel");
+let sliders = [];
+
+let slideIndex = 0; //track the current slideIndex
+
+const createSlide = () => {
+  if (slideIndex >= movies.length) {
+    slideIndex = 0;
+  }
+
+  //create DOM Elements
+  let slide = document.createElement("div");
+  var imgElement = document.createElement("img");
+  let content = document.createElement("div");
+  let h1 = document.createElement("h1");
+  let p = document.createElement("p");
+
+  // attaching all elements
+  imgElement.appendChild(document.createTextNode(""));
+  h1.appendChild(document.createTextNode(movies[slideIndex].name));
+  p.appendChild(document.createTextNode(movies[slideIndex].des));
+  content.appendChild(h1);
+  content.appendChild(p);
+  slide.appendChild(content);
+  slide.appendChild(imgElement);
+  caraousel.appendChild(slide);
+
+  // imgElement.appendChild(document.createTextNode(""));
+  // h1.appendChild(document.createTextNode(movies[slideIndex].name));
+  // p.appendChild(document.createTextNode(movies[slideIndex].des));
+  // content.appendChild(h1);
+  // content.appendChild(p);
+  // slide.appendChild(content);
+  // slide.appendChild(imgElement);
+  // caraousel.appendChild(slide);
+
+  //setting up images
+  imgElement.src = movies[slideIndex].image;
+  slideIndex++;
+
+  //setting elements classnames
+  slide.className = "slider";
+  content.className = "slide-content";
+  h1.className = "movie-title";
+  p.className = "movie-des";
+
+  sliders.push(slide);
+
+  if (sliders.length) {
+    sliders[0].style.marginLeft = `calc(-${100 * (sliders.length - 2)}% - ${
+      30 * (sliders.length - 2)
+    }px)`;
+  }
+};
+
+for (let i = 0; i < 3; i++) {
+  createSlide();
+}
+
+setInterval(() => {
+  createSlide();
+}, 3000);
+
+//Video Cards
+
+const videoCards = [...document.querySelectorAll(".video-card")];
+
+videoCards.forEach((item) => {
+  item.addEventListener("mouseover", () => {
+    let video = item.children[1];
+    video.play();
+  });
+  item.addEventListener("mouseleave", () => {
+    let video = item.children[1];
+    video.pause();
+  });
+});
+
+// card sliders
+
+let cardContainers = [...document.querySelectorAll(".card-container")];
+let preBtns = [...document.querySelectorAll(".pre-btn")];
+let nxtBtns = [...document.querySelectorAll(".nxt-btn")];
+
+cardContainers.forEach((item, i) => {
+  let containerDimensions = item.getBoundingClientRect();
+  let containerWidth = containerDimensions.width;
+
+  nxtBtns[i].addEventListener("click", () => {
+    item.scrollLeft += containerWidth - 200;
+  });
+
+  preBtns[i].addEventListener("click", () => {
+    item.scrollLeft -= containerWidth + 200;
+  });
+
+  // let cardContainers = [...document.querySelectorAll(".card-container")];
+  // let preBtns = [...document.querySelectorAll(".pre-btn")];
+  // let nxtBtns = [...document.querySelectorAll(".nxt-btn")];
+
+  // cardContainers.forEach((item, i) => {
+  //   let containerDimensions = item.getBoundingClientRect();
+  //   let containerWidth = containerDimensions.width;
+
+  // nxtBtns[i].addEventListener("click", () => {
+  //   item.scrollLeft += containerWidth - 200;
+  // });
+
+  // preBtns[i].addEventListener("click", () => {
+  //   item.scrollLeft -= containerWidth + 200;
+  // });
+});
